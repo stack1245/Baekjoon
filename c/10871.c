f8aa28105c5d58a9e4b91df3d506f9475776f6a7
stack1245 <halleiqd1937@gmail.com>
2025-12-06 23:31:05 +0900

    10871

---
+++ c/10871.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int N, X;
+    scanf("%d %d", &N, &X);
+
+    int A[10001];
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &A[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        if (A[i] < X)
+        {
+            printf("%d ", A[i]);
+        }
+    }
+
+    return 0;
+}
