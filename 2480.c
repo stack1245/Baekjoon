6d8339f4faf537f0665885980c5f013c0bf1c603
stack1245 <halleiqd1937@gmail.com>
2025-12-06 15:29:30 +0900

    2480

---
+++ 2480.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+    int prize = 0;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a == b && b == c)
+    {
+        prize = 10000 + a * 1000;
+    }
+    else if (a == b || a == c)
+    {
+        prize = 1000 + a * 100;
+    }
+    else if (b == c)
+    {
+        prize = 1000 + b * 100;
+    }
+    else
+    {
+        int max = a;
+        if (b > max)
+            max = b;
+        if (c > max)
+            max = c;
+        prize = max * 100;
+    }
+
+    printf("%d\n", prize);
+
+    return 0;
+}
