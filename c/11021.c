cdaf1d56c8e21c9231915fb30c56c1e5e2179248
stack1245 <halleiqd1937@gmail.com>
2025-12-06 15:49:14 +0900

    11021

--- c/11021.c
+++ c/11021.c
@@ -8,7 +8,7 @@ int main()
     for (int i = 1; i <= T; i++)
     {
         scanf("%d %d", &A, &B);
-        printf("Case #%d: %d + %d = %d\n", i, A, B, A + B);
+        printf("Case #%d: %d\n", i, A + B);
     }
 
     return 0;
