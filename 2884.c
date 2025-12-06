b4f6fadb29244807cfde9dbfcfc21bf12ccb176f
stack1245 <halleiqd1937@gmail.com>
2025-12-06 15:23:42 +0900

    2884

---
+++ 2884.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int H, M;
+    scanf("%d %d", &H, &M);
+
+    M -= 45;
+
+    if (M < 0)
+    {
+        M += 60;
+        H -= 1;
+
+        if (H < 0)
+        {
+            H = 23;
+        }
+    }
+
+    printf("%d %d\n", H, M);
+
+    return 0;
+}
